da50cadced4400e0711baeb16f8b465b367357f3
rokas_simon <rokas.rokanas@gmail.com>
2022-11-10 16:23:32 +0200

    Added NUnit test project

---
+++ TVShowApplication.Tests/UnitTests/JwtGeneratorTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using TVShowApplication.Data.Options;
+using TVShowApplication.Services.Authentication;
+using TVShowApplication.Services.Interfaces;
+
+namespace TVShowApplication.Tests.UnitTests
+{
+    internal class JwtGeneratorTests
+    {
+        private JwtOptions _jwtOptions;
+        private IJwtGenerator _jwtGenerator;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _jwtOptions = new JwtOptions
+            {
+                Audience = "test",
+                Issuer = "test",
+                ExpirationSeconds = 60,
+                RefreshTokenExpirationDays = 1,
+                Secret = "secret",
+            };
+            _jwtGenerator = new JwtGenerator(Options.Create(_jwtOptions));
+        }
+
+        [Test]
+        public void GetPrinciap_GivenInvalidJwtToken_ThrowsArgumentException()
+        {
+            var invalidSignedToken = "this is not a JWT";
+
+            var act = () => _jwtGenerator.GetPrincipalFromExpiredToken(invalidSignedToken);
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void GetPrincipal_GivenTokenWithInvalidSigningKey_ThrowsSecurityTokenException()
+        {
+            var invalidSignedToken = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ0ZXN0IiwiYXVkIjoidGVzdCJ9.7UPAgfoVuHkcGTaZqlMOgqx_0VG9MMuiiWB7JRSEDMMuqbXS8Dfnm5-n4XhwWWebIkvedcGtRcsB2TvI_aRi9Q";
+
+            var act = () => _jwtGenerator.GetPrincipalFromExpiredToken(invalidSignedToken);
+
+            act.Should().Throw<SecurityTokenException>();
+        }
+    }
+}
