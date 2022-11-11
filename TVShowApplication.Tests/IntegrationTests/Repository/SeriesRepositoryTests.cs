5bb6d77285a9c92a3cd024bf1324c25c5efb4ac8
rokas_simon <rokas.rokanas@gmail.com>
2022-11-11 09:04:40 +0200

    Added series repository tests

---
+++ TVShowApplication.Tests/IntegrationTests/Repository/SeriesRepositoryTests.cs
@@ -0,0 +1,210 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using TVShowApplication.Data;
+using TVShowApplication.Exceptions;
+using TVShowApplication.Models;
+using TVShowApplication.Services.Authentication;
+using TVShowApplication.Services.Database;
+using TVShowApplication.Services.Interfaces;
+
+namespace TVShowApplication.Tests.IntegrationTests.Repository
+{
+    internal class SeriesRepositoryTests
+    {
+        private TVShowContext _context;
+        private ISeriesRepository _seriesRepository;
+        private Genre _genre;
+        private Administrator _admin;
+
+        private Series? _createdSeries;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var contextOptions = new DbContextOptionsBuilder<TVShowContext>()
+                .UseInMemoryDatabase("TVShowApplication")
+                .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            _context = new TVShowContext(contextOptions);
+            _context.Database.EnsureDeleted();
+
+            _admin = new Administrator
+            {
+                Id = 1,
+                Email = "test@gmail.com",
+                HashedPassword = "fbaojalfnafljb",
+                RefreshToken = "ajfbalfmn",
+                RefreshTokenExpiryTime = DateTime.Today.AddDays(2),
+                Salt = "ofgahnfa",
+                PostedSeries = new List<Series>(),
+                Reviews = new List<Review>(),
+            };
+
+            _genre = new Genre
+            {
+                Id = 1,
+                Name = "Test",
+                Description = ".",
+                Videos = new List<Series>(),
+            };
+
+            _context.Admins.Add(_admin);
+            _context.Genres.Add(_genre);
+            _context.SaveChanges();
+
+            _seriesRepository = new SeriesRepository(_context, new UserDataProvider { UserId = _admin.Id, UserRole = Models.Role.Admin });
+        }
+
+        [Test, Order(1)]
+        public async Task InsertSeriesAsync_ValidGenreAndSeries_ReturnsCreatedSeries()
+        {
+            var seriesToCreate = new Series
+            {
+                Id = 1,
+                Name = "Test",
+                Description = ".",
+                CoverImagePath = ".",
+                StarringCast = new[] { "c" },
+                Directors = new[] { "d" },
+                Poster = _admin,
+                Reviews = new List<Review>(),
+                Genres = new List<Genre> { _genre },
+            };
+
+            _createdSeries = await _seriesRepository.InsertSeriesAsync(_genre.Id, seriesToCreate);
+
+            _createdSeries.Should().NotBeNull();
+        }
+
+        [Test, Order(2)]
+        public async Task GetSeriesAsync_ValidGenreAndSeries_ReturnsExpectedSeries()
+        {
+            var seriesToGet = _createdSeries!.Id;
+
+            var series = await _seriesRepository.GetSeriesAsync(_genre.Id, seriesToGet);
+
+            series.Should().NotBeNull();
+            series.Should().BeEquivalentTo(_createdSeries);
+        }
+
+        [Test, Order(3)]
+        public async Task GetSeriesAsync_ReturnsExpectedSeries()
+        {
+            var series = await _seriesRepository.GetSeriesAsync(_genre.Id);
+
+            series.Should().NotBeNull();
+            series.Should().BeEquivalentTo(new[] { _createdSeries });
+        }
+
+        [Test, Order(4)]
+        public async Task UpdateSeriesAsync_GivenValidGenreAndSeriesUpdate_ReturnsSuccess()
+        {
+            const string newDescription = "New Name";
+            var seriesToUpdate = new Series
+            {
+                Description = newDescription,
+                Directors = new[] { "Christopher Nolan" },
+                StarringCast = new[] { "Leonardo DiCaprio" },
+            };
+
+            var success = await _seriesRepository.UpdateSeriesAsync(_genre.Id, _createdSeries!.Id, seriesToUpdate);
+
+            success.Should().BeTrue();
+            _createdSeries!.Description.Should().Be(newDescription);
+        }
+
+        [Test, Order(5)]
+        public async Task DeleteSeriesAsync_GivenValidGenreAndSeries_ReturnsSuccess()
+        {
+            var seriesToDelete = _createdSeries!.Id;
+
+            var success = await _seriesRepository.DeleteSeriesAsync(_genre.Id, seriesToDelete);
+
+            success.Should().BeTrue();
+        }
+
+        [Test, Order(6)]
+        public async Task GetSingleSeriesAsync_GivenInvalidGenre_ThrowsNotFound()
+        {
+            var act = () => _seriesRepository.GetSeriesAsync(9999, _createdSeries!.Id);
+
+            await act.Should().ThrowAsync<ResourceNotFoundException>();
+        }
+
+        [Test, Order(7)]
+        public async Task GetSeriesAsync_GivenInvalidGenre_ThrowsNotFound()
+        {
+            var act = () => _seriesRepository.GetSeriesAsync(9999);
+
+            await act.Should().ThrowAsync<ResourceNotFoundException>();
+        }
+
+        [Test, Order(8)]
+        public async Task GetSingleSeriesAsync_GivenValidGenreAndInvalidSeries_ReturnsNull()
+        {
+            var series = await _seriesRepository.GetSeriesAsync(_genre.Id, 9999);
+
+            series.Should().BeNull();
+        }
+
+        [Test, Order(9)]
+        public async Task UpdateSeriesAsync_GivenInvalidGenreId_ThrowsNotFound()
+        {
+            const string newDescription = "New Name";
+            var oldDescription = _createdSeries!.Description;
+            var seriesToUpdate = new Series
+            {
+                Description = newDescription,
+                Directors = new[] { "Christopher Nolan" },
+                StarringCast = new[] { "Leonardo DiCaprio" },
+            };
+
+            var act = () => _seriesRepository.UpdateSeriesAsync(9999, seriesToUpdate.Id, seriesToUpdate);
+
+            await act.Should().ThrowAsync<ResourceNotFoundException>();
+            _createdSeries.Description.Should().Be(oldDescription);
+        }
+
+        [Test, Order(10)]
+        public async Task UpdateSeriesAsync_GivenValidGenreAndInvalidSeries_ThrowsNotFound()
+        {
+            const string newDescription = "New Name";
+            var oldDescription = _createdSeries!.Description;
+            var seriesToUpdate = new Series
+            {
+                Description = newDescription,
+                Directors = new[] { "Christopher Nolan" },
+                StarringCast = new[] { "Leonardo DiCaprio" },
+            };
+
+            var act = () => _seriesRepository.UpdateSeriesAsync(_genre.Id, 9999, seriesToUpdate);
+
+            await act.Should().ThrowAsync<ResourceNotFoundException>();
+            _createdSeries.Description.Should().Be(oldDescription);
+        }
+
+        [Test, Order(11)]
+        public async Task DeleteSeriesAsync_GivenInvalidGenre_ThrowsNotFoundException()
+        {
+            var act = () => _seriesRepository.DeleteSeriesAsync(9999, _createdSeries!.Id);
+
+            await act.Should().ThrowAsync<ResourceNotFoundException>();
+        }
+
+        [Test, Order(12)]
+        public async Task DeleteSeriesAsync_GivenValidGenreAndInvalidSeries_ThrowsNotFoundException()
+        {
+            var act = () => _seriesRepository.DeleteSeriesAsync(_genre.Id, 9999);
+
+            await act.Should().ThrowAsync<ResourceNotFoundException>();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+    }
+}
--- TVShowApplication/Services/Database/SeriesRepository.cs
+++ TVShowApplication/Services/Database/SeriesRepository.cs
@@ -18,49 +18,6 @@ namespace TVShowApplication.Services.Database
             _userDataProvider = userDataProvider;
         }
 
-        //public async Task<Series?> InsertSeriesAsync(Series series)
-        //{
-        //    Fault.IfMissingRole(_userDataProvider.UserRole, Role.Admin, Role.Poster);
-
-        //    var genreIds = series.Genres.Select(series => series.Id);
-        //    var genresFromDb = await _context.Genres
-        //        .Where(g => genreIds.Contains(g.Id))
-        //        .ToListAsync();
-
-        //    if (series.Genres.Count != genresFromDb.Count) return null;
-        //    series.Genres = genresFromDb;
-
-        //    var posterFromDb = await _context.Posters.SingleOrDefaultAsync(p => p.Id == series.Poster.Id);
-        //    if (posterFromDb == null) return null;
-        //    series.Poster = posterFromDb;
-
-        //    var createdSeries = await _context.Series.AddAsync(series);
-        //    var successfullyCreated = await SaveAsync();
-
-        //    if (successfullyCreated)
-        //    {
-        //        return createdSeries.Entity;
-        //    }
-
-        //    return null;
-        //}
-
-        //public async Task<bool> UpdateSeriesAsync(int id, Series series)
-        //{
-        //    Fault.IfMissingRole(_userDataProvider.UserRole, Role.Admin, Role.Poster);
-
-        //    var seriesToUpdate = await _context.Series.SingleOrDefaultAsync(x => x.Id == id);
-        //    if (seriesToUpdate == null) return false;
-
-        //    seriesToUpdate.Description = series.Description;
-        //    seriesToUpdate.Directors = series.Directors;
-        //    seriesToUpdate.StarringCast = series.StarringCast;
-
-        //    _context.Series.Update(seriesToUpdate);
-
-        //    return await SaveAsync();
-        //}
-
         private async Task<bool> SaveAsync()
         {
             return await _context.SaveChangesAsync() != 0;
